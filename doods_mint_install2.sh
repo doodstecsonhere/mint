d1e3afb540aa01bfc9997588951b3538a03f47e9
Doods Tecson <emmanuel.tecson@gmail.com>
2024-03-18 18:20:05 +0800

    Update doods_mint_install2.sh

--- doods_mint_install2.sh
+++ doods_mint_install2.sh
@@ -36,47 +36,6 @@ sudo apt install -y -f
 #install Bavarder
 flatpak install -y flathub io.github.Bavarder.Bavarder
 
-#normcap pypi
-
-# install pip
-sudo apt install -y python3-pip
-# Install dependencies (Ubuntu/Debian)
-sudo apt install -y build-essential tesseract-ocr tesseract-ocr-eng libtesseract-dev libleptonica-dev wl-clipboard
-# Install normcap
-pip install normcap
-#fix $PATH
-echo 'export PATH=$PATH:/home/doods/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-
-echo '#!/bin/bash
-/home/doods/.local/bin/normcap' > run_normcap.sh 
-chmod +x ~/run_normcap.sh
-
-#microsoft-edge-stable-bin
-sudo apt update -y
-sudo add-apt-repository universe
-sudo wget https://packages.microsoft.com/keys/microsoft.asc -O microsoft.gpg
-sudo apt key add microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-stable.list'
-sudo rm microsoft.gpg
-sudo apt update -y
-sudo apt install microsoft-edge-stable
-
-#clean-up files
-sudo rm -rf 
-~/Linux_terminal_color.zip 
-~/google-chrome-stable_current_amd64.deb 
-~/megasync-xUbuntu_22.04_amd64.deb 
-~/PimpMyStremio-linux.zip 
-~/rescuetime_current_amd64.deb 
-~/stremio-service_amd64.deb 
-~/Stremio_v4.4.165.deb 
-~/teamviewer_amd64.deb 
-~/zoom_amd64.deb 
-#clean-up folders
-sudo rm -rf 
-~/timeshift-autosnap-apt && \
-
 #maintenance
 sudo apt autoremove --purge -y && sudo apt clean && sudo apt purge $(dpkg -l | awk '/^rc/ { print $2 }') && sudo journalctl --vacuum-size=50M && flatpak uninstall --unused
 
